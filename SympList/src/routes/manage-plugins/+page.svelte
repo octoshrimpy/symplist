9b3a71d6ceb31b5c5896f98a87aea776de7a3397
Dev-Corinne <corinne.m.dev@gmail.com>
2022-09-16 19:18:20 -0400

    deleted unnecessary files

--- SympList/src/routes/manage-plugins/+page.svelte
+++
@@ -1,5 +0,0 @@
-<svelte:head>
-	<title>Tracker-Info</title>
-	<link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.classless.min.css">
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
